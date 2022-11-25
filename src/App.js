f572cb5daa771471fd708fe852859a3f6f146b8c
João Gonçalves <joao.n.d.goncalves@gmail.com>
2022-11-25 09:40:41 +0000

    new header menu items

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Signup from "./pages/Signup";
-import Bankaccounts from "./pages/Bankaccounts";
-import Personal from "./pages/Personal";
+import Personal from "./components/personal/Personal";
 import Settings from "./pages/Settings";
 import Transaction from "./pages/Transaction";
 import LoginUser from "./pages/LoginUser";
 import Header from './components/header/Header';
+import BankAccounts from './components/bankAccounts/BankAccounts';
 
 function App() {
   return (
@@ -16,13 +16,13 @@ function App() {
         <Routes>
           {/* Change to the component you are developing */}
           <Route path="/" element={<Header />} />
-          {/* <Route path="/" element={<Layout />} />
+          {/* <Route path="/" element={<Layout />} /> */}
           <Route path="Signup" element={<Signup />} />
           <Route path="LoginUser" element={<LoginUser />} />
-          <Route path="Bankaccounts" element={<Bankaccounts />} />
-          <Route path="Personal" element={<Personal />} />
+          <Route path="bankAccounts" element={<BankAccounts />} />
+          <Route path="personal" element={<Personal />} />
           <Route path="user/Settings" element={<Settings />} />
-          <Route path="Transaction" element={<Transaction />} /> */}
+          <Route path="Transaction" element={<Transaction />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,16 @@ export default function Header() {
       <Navbar bg="primary" variant="dark">
         <Container>
           <Nav className="ml-auto">
-            <NavDropdown title="Menu" id="basic-nav-dropdown" />
+            <NavDropdown title="Menu" id="basic-nav-dropdown">
+              {/* NavDropdown children item */}
+              <NavDropdown.Item href="/">Home</NavDropdown.Item>
+              <NavDropdown.Item href="personal">My Account</NavDropdown.Item>
+              <NavDropdown.Item href="bankAccounts">
+                Bank Accounts
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/">Notifications</NavDropdown.Item>
+              <NavDropdown.Item href="/">Logout</NavDropdown.Item>
+            </NavDropdown>
             <Nav.Link href="/">Notifications</Nav.Link>
             <Button variant="primary">$ New</Button>
           </Nav>
